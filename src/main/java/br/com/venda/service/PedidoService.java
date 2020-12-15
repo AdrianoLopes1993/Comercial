7ef02d76b728363353506d376fce1370dac9ca3a
AdrianoLopes1993 <adriano.rocha.lopes@icloud.com>
2020-12-15 00:25:40 -0300

    Revisão da classe PedidoService.

--- src/main/java/br/com/venda/service/PedidoService.java
+++ src/main/java/br/com/venda/service/PedidoService.java
@@ -28,32 +28,6 @@ public class PedidoService {
 	@Value("${url.consulta.produto}")
 	private String consultaProduto;
 
-	public Pedido salvarPedido(Pedido pedido) {
-
-		Long idCliente = pedido.getCodigoCliente();
-		ClienteDTO dtoCliente = rt.getForObject(consultaCliente + "/" + idCliente, ClienteDTO.class);
-
-		Long idProduto = pedido.getCodigoProduto();
-		ProdutoDTO dtoProduto = rt.getForObject(consultaProduto + "/" + idProduto, ProdutoDTO.class);
-
-		Pedido ped = new Pedido();
-		ped.setCodigoCliente(pedido.getCodigoCliente());
-		ped.setCpf(dtoCliente.getCpf());
-		ped.setNomeCliente(dtoCliente.getNomeCliente());
-		ped.setTelefone(dtoCliente.getTelefone());
-
-		ped.setCodigoProduto(pedido.getCodigoProduto());
-		ped.setCusto(dtoProduto.getCusto());
-		ped.setFornecedor(dtoProduto.getFornecedor());
-		ped.setNomeProduto(dtoProduto.getNomeProduto());
-		ped.setValorFinal(dtoProduto.getValorFinal());
-
-		ped.setFormaPagamento(pedido.getFormaPagamento());
-
-		return repository.save(ped);
-
-	}
-
 	public List<Pedido> listarPedido() {
 		return repository.findAll();
 	}
@@ -66,4 +40,29 @@ public class PedidoService {
 		repository.deleteById(id);
 	}
 
+	public Pedido salvarPedido(Pedido pedido) {
+
+		Long idCliente = pedido.getCodigoCliente();
+		ClienteDTO objectCliente = rt.getForObject(consultaCliente + "/" + idCliente, ClienteDTO.class);
+
+		Long idProduto = pedido.getCodigoProduto();
+		ProdutoDTO objectProduto = rt.getForObject(consultaProduto + "/" + idProduto, ProdutoDTO.class);
+
+		Pedido ped = new Pedido();
+		ped.setCodigoCliente(pedido.getCodigoCliente());
+		ped.setNomeCliente(objectCliente.getNomeCliente());
+		ped.setCpf(objectCliente.getCpf());
+		ped.setTelefone(objectCliente.getTelefone());
+
+		ped.setCodigoProduto(pedido.getCodigoProduto());
+		ped.setFornecedor(objectProduto.getFornecedor());
+		ped.setNomeProduto(objectProduto.getNomeProduto());
+		ped.setCusto(objectProduto.getCusto());
+		ped.setValorFinal(objectProduto.getValorFinal());
+
+		ped.setFormaPagamento(pedido.getFormaPagamento());
+
+		Pedido save = repository.save(ped);
+		return save;
+	}
 }
